b4356f0378ef2932c43bcf1a1a458f92a479c3b9
illusionX07 <82873234+illusionX07@users.noreply.github.com>
2022-03-21 15:03:59 +0530

    Update alive.py

--- RiZoeLXSpam/plugins/alive.py
+++ RiZoeLXSpam/plugins/alive.py
@@ -1,14 +1,14 @@
-from .. import Riz, SUDO_USERS, rizoelversion
+from .. import Riz, SUDO_USERS, lightyagamiversion
 from .. import ALIVE_PIC
 from telethon import events, version
 from time import time
 from datetime import datetime
 
-RIZ_PIC = ALIVE_PIC if ALIVE_PIC else "https://telegra.ph/file/ba87c58f01a6fcb5ef512.jpg"
+RIZ_PIC = ALIVE_PIC if ALIVE_PIC else ""
   
 
           
-rizoel = "✧ 𝑅𝐼𝑍𝑂𝐸𝐿 𝑋 𝑆𝑃𝐴𝑀 𝐼𝑍𝑍 𝐴𝐿𝐼𝑉𝐸𝐸 ✧\n\n"
+rizoel = "✧ 𝐿𝐼𝐺𝐻𝑇𝑌𝐴𝐺𝐴𝑀𝐼 𝑋 𝑆𝑃𝐴𝑀𝑀𝐸𝑅 𝐼𝑍 𝐴𝐿𝐼𝑉𝐸 ✧\n\n"
 
 rizoel += f"┏━━━━━━━━━━━━━━━━━━━\n"
 
@@ -16,15 +16,15 @@ rizoel += f"┣➣ **ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ** : `3.9.6`\n"
 
 rizoel += f"┣➣ **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ** : `{version.__version__}`\n"
 
-rizoel += f"┣➣ **ʀɪᴢᴏᴇʟXsᴘᴀᴍ ᴠᴇʀsɪᴏɴ**  : `{rizoelversion}`\n"
+rizoel += f"┣➣ ** ʟɪɢʜᴛʏᴀɢᴀᴍɪ x sᴘᴀᴍ ᴠᴇʀsɪᴏɴ**  : `{rizoelversion}`\n"
     
-rizoel += f"┣➣ **sᴜᴘᴘᴏʀᴛ** : [JOIN](https://t.me/DNHxHELL)\n"
+rizoel += f"┣➣ **sᴜᴘᴘᴏʀᴛ** : [MESSAGE](https://t.me/illusion_07)\n"
 
-rizoel += f"┣➣ **ᴄʜᴀɴɴᴇʟ** : [JOIN](https://t.me/RiZoeLX)\n"
+rizoel += f"┣➣ **ᴄᴏɴᴛᴀᴄᴛ** : [MESSAGE](https://t.me/illusion_07)\n"
 
 rizoel += f"┗━━━━━━━━━━━━━━━━━━━\n\n"
 
-rizoel += f"🖤 [𝐑𝐄𝐏𝐎](https://github.com/MrRizoel/RiZoeLXSpam) 🖤"            
+rizoel += f"🖤 [TG](https://https://t.me/illusion_07) 🖤"            
                                     
 @Riz.on(events.NewMessage(pattern=r"\.alive"))
 async def alive(event):

80ff994079fc38b5e7abdfb71e5cc0ccf5ddf896
illusionX07 <82873234+illusionX07@users.noreply.github.com>
2022-03-21 15:15:50 +0530

    Update updater.py

--- RiZoeLXSpam/plugins/updater.py
+++ RiZoeLXSpam/plugins/updater.py
@@ -12,12 +12,12 @@ IS_SELECTED_DIFFERENT_BRANCH = (
     "in this case, Updater is unable to identify the branch to be updated."
     "please check out to an official branch, and re-start the updater."
 )
-OFFICIAL_UPSTREAM_REPO = "https://github.com/MrRizoel/RiZoeLXSpam"
+OFFICIAL_UPSTREAM_REPO = "https://github.com/illusionX07/infggffvv"
 BOT_IS_UP_TO_DATE = "**The RiZoeL X Spam** is up-to-date sur."
 NEW_BOT_UP_DATE_FOUND = (
     "new update found for {branch_name}\n"
     "changelog: \n\n{changelog}\n"
-    "updating your RiZoeL X Spam ..."
+    "updating your LighTYaGamI X Spam ..."
 )
 NEW_UP_DATE_FOUND = "New update found for {branch_name}\n" "`updating your RiZoeL X Spam...`"
 REPO_REMOTE_NAME = "temponame"
@@ -50,7 +50,7 @@ RESTARTING_APP = "re-starting heroku application"
 @Riz20.on(events.NewMessage(pattern=r"\.update"))
 async def restart(e):
     if e.sender_id in SUDO_USERS:
-        text = "__U𝗽𝗱𝗮𝘁𝗶𝗻𝗴..... 𝗬𝗼𝘂𝗿 𝗥𝗶𝗭𝗼𝗲𝗟 𝗫 𝗦𝗽𝗮𝗺 𝗨𝘀𝗲𝗿𝗯𝗼𝘁𝘀__\n𝗧𝘆𝗽𝗲 .ping 𝗔𝗳𝘁𝗲𝗿 5𝗺𝗶𝗻𝘀 𝗧𝗼 𝗰𝗵𝗲𝗰𝗸 𝗜'𝗺 𝗼𝗻 !!"
+        text = "__U𝗽𝗱𝗮𝘁𝗶𝗻𝗴..... 𝗬𝗼𝘂𝗿 LiGhTYaGamI 𝗫 𝗦𝗽𝗮𝗺 𝗨𝘀𝗲𝗿𝗯𝗼𝘁𝘀__\n𝗧𝘆𝗽𝗲 .ping 𝗔𝗳𝘁𝗲𝗿 5𝗺𝗶𝗻𝘀 𝗧𝗼 𝗰𝗵𝗲𝗰𝗸 𝗜'𝗺 𝗼𝗻 !!"
         await e.reply(text, parse_mode=None, link_preview=None)
 
 
